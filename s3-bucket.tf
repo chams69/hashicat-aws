412e40a989b8ffa59fa135b16409d8a3d74c4cab
ch <ch_zemzemi@hotmail.fr>
2023-09-06 15:27:31 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,9 +1,10 @@
 module "s3_bucket" {
   source  = "app.terraform.io/org-hcp-cze/s3-bucket/aws"
-  version = "3.8.0"
-  bucket = "my-s3-bucket-chamDevo"
+  version = "2.8.0"
+  
+  bucket = "my-s3"
   acl    = "private"
-  bucket_prefix = "prefixsomething"
+  bucket_prefix = "chamseddine-zemzemi"
 
   control_object_ownership = true
   object_ownership         = "ObjectWriter"
