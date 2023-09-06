8a3cbd7270a1a20d1f9e3aed1e1e815afa92b652
ch <ch_zemzemi@hotmail.fr>
2023-09-06 14:28:42 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,8 +1,9 @@
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
+  source  = "app.terraform.io/org-hcp-cze/s3-bucket/aws"
+  version = "2.15.0"
   bucket = "my-s3-bucket-chamDevo"
   acl    = "private"
+  bucket_prefix = "prefixsomething"
 
   control_object_ownership = true
   object_ownership         = "ObjectWriter"
